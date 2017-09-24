a9727a35661b792533809094a3756288a34bdb15
Michael Zhang <failed.down@gmail.com>
2017-09-24 01:49:27 -0500

    Added tabs layout, built on real Android device.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,34 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { TabsPage } from '../pages/tabs/tabs';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+
+    AboutPage,
+    ContactPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+      IonicModule.forRoot(MyApp, { tabsPlacement: 'bottom' })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+
+    AboutPage,
+    ContactPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
